7775a1c56b645b1378929c33bd5f3ae939a55c01
Eyader Tsehayu <eyadertsehayu@gmail.com>
2026-03-14 14:08:00 +0300

    Add shared constants including country data, dashboard menu items, and role enums

---
+++ Frontend/src/shared/constants/dashboard/items.tsx
@@ -0,0 +1,85 @@
+// import { UserOutlined, DashboardOutlined } from "@ant-design/icons";
+import {
+  MdOutlineDashboard,
+  MdOutlinePersonOutline,
+  MdOutlinePerson3,
+  MdOutlineAdminPanelSettings,
+  MdOutlineApproval,
+} from "react-icons/md";
+import { LuSchool } from "react-icons/lu";
+import { TbMapDollar } from "react-icons/tb";
+import { NavigateFunction } from "react-router-dom";
+import { FaHandHoldingDollar, FaSackDollar } from "react-icons/fa6";
+import { IoAnalyticsSharp } from "react-icons/io5";
+import { EnumRoles } from "../enum/app.enum";
+export const getMenuItems = (
+  navigate: NavigateFunction,
+  selectedKey: string
+) => {
+  const iconSize = 18; // Increased icon size (default is usually around 14-16px)
+
+  return [
+    {
+      key: "dashboard",
+      icon: <MdOutlineDashboard size={iconSize} />,
+      label: "Dashboard",
+      onClick: () => {
+        navigate("/");
+      },
+    },
+    {
+      key: "Skill",
+      icon: <LuSchool size={iconSize} />,
+      label: "Skill",
+      // roles: [EnumRoles.SuperAdmin],
+      onClick: () => {
+        navigate("skills");
+      },
+      className: selectedKey === "skill" ? "bg-primary text-white" : "",
+    },
+    {
+      key: "Location",
+      icon: <FaSackDollar size={iconSize} />,
+      label: "Location",
+      // roles: [EnumRoles.SuperAdmin],
+      onClick: () => {
+        navigate("locations");
+      },
+    },
+    {
+      key: "User",
+      icon: <MdOutlinePersonOutline size={iconSize} />,
+      label: "User",
+      // roles: [EnumRoles.SuperAdmin],
+      onClick: () => {
+        navigate("users");
+      },
+    },
+    // {
+    //   key: "User Management",
+    //   icon: <MdOutlinePerson3 size={iconSize} />,
+    //   label: "User Management",
+    //   roles: [EnumRoles.SuperAdmin],
+    //   children: [
+    //     {
+    //       key: "users",
+    //       icon: <MdOutlinePersonOutline size={iconSize} />,
+    //       label: "Users",
+    //       roles: [EnumRoles.SuperAdmin],
+    //       onClick: () => {
+    //         navigate("users");
+    //       },
+    //     },
+
+    //   ],
+    // },
+    // {
+    //   key: "Report",
+    //   icon: <IoAnalyticsSharp size={iconSize} />,
+    //   label: "Report & Analytics",
+    //   onClick: () => {
+    //     navigate("reports");
+    //   },
+    // },
+  ];
+};
---
+++ Frontend/src/shared/constants/enum/app.enum.ts
@@ -0,0 +1,12 @@
+enum EnumRoles {
+  Admin = "Admin",
+  Manager = "Manager",
+  Staff = "Staff",
+}
+
+enum EnumTimeZones {
+  NewYork = "America/New_York",
+  LosAngeles = "America/Los_Angeles",
+}
+
+export { EnumRoles, EnumTimeZones }
