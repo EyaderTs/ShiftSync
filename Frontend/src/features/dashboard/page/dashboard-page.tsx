45e063c000d845b7f4316c614349cc573631343e
Eyader Tsehayu <eyadertsehayu@gmail.com>
2026-03-14 14:14:40 +0300

    Add dashboard feature with endpoint configuration, page component, and routing setup

---
+++ Frontend/src/features/dashboard/page/dashboard-page.tsx
@@ -0,0 +1,10 @@
+import { CollectionQuery } from '@/models/collection.model';
+import React, { useEffect, useState } from 'react';
+
+export default function DashboardPage() {
+  return (
+    <div className='bg-white px-6  min-h-screen'>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
---
+++ Frontend/src/features/dashboard/route/dashboard-route.tsx
@@ -0,0 +1,7 @@
+import DashboardPage from "@/features/dashboard/page/dashboard-page";
+
+// eslint-disable-next-line import/prefer-default-export
+export const DashboardRoute = {
+  path: "/",
+  element: <DashboardPage />,
+};
